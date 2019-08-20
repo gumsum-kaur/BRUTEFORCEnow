7e2a42e1938cb463edbd9b49b5a4db6ae171c78d
Gumsum Kaur <54205121+username1437@users.noreply.github.com>
2019-08-20 18:41:56 +0530

    Add files via upload

---
+++ pswrdkill.py
@@ -0,0 +1,60 @@
+thistuple = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z')
+
+thistuple1=('a' , 'b', 'c', 'd', 'e', 'f','g', 'h','i','j','k', 'l', 'm', 'n', 'o','p','q','r','s','t','u','v','w','x','y','z')
+
+thistuple2= ('0123456789')
+
+thistuple3 = ("+-*/?!:( )[]{}<>=^%$|_;#@\&©®₹£€¥¡¿")
+
+#now run the commands********
+
+print (" Enter any capital letter !", '\n')
+
+x = input()
+
+if x in thistuple :
+	print("yes ! your input is accessible.")
+	
+elif x in thistuple1:
+	print("Opps ! You have entered a lower-case letter !", '\n')
+	print(" strike the ENTER key, to get data !")
+	input()
+	mytuple = thistuple1.count(x)
+	myindex = thistuple1.index(x)
+	print('\n', thistuple1)
+	print('\n')
+	print('Identity :', x , '\n')
+	print('No. of letters : ' ,mytuple, '\n',  'Position   No.  : ' ,myindex)
+	exit()
+	
+elif x in thistuple2:
+	print('\n', " Opps ! You have entered a digit !", "\n")
+	print("strike the ENTER key, to get data !")
+	input()
+	mytuple = thistuple2.count(x)
+	myindex = thistuple2.index(x)
+	print('\n', thistuple2)
+	print('\n')
+	print('Identity :', x , '\n')
+	print('No. of digits : ' ,mytuple, '\n',  'Position   No.  : ' ,myindex)
+	exit()
+
+elif x in thistuple3 :
+	print('\n', " Opps ! You have entered some special characters !", '\n')
+	print("strike the ENTER key to get data !")
+	input()
+	mytuple = thistuple3.count(x)
+	myindex = thistuple3.index(x)
+	print('\n', thistuple3)
+	print('\n')
+	print('Identity :', x , '\n')
+	print('Special Character Repeat times: ' ,mytuple, '\n',  'Position   No.  : ' ,myindex)
+	exit()
+
+
+mytuple = thistuple.count(x)
+myindex = thistuple.index(x)
+print('\n', thistuple)
+print('\n')
+print('Identity :', x , '\n')
+print('No. of letters : ' ,mytuple, '\n',  'Position   No.  : ' ,myindex)
